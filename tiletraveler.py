236ccc3956bb086ee3a8552ab5bd53dcfade799b
Jóel Snær Garcia <joelsnaer@Joels-MacBook-Pro.local>
2018-09-18 09:13:33 +0000

    Skrifað upp algrím

---
+++ tiletraveler.py
@@ -0,0 +1,6 @@
+# Forritið felst í því að notandinn byrjar á 1,1 og þarf að komast til 3,1 
+# Forritið keyrist í while loopu alveg þangað til að notandinn kemst á endareitinn
+# Það er skoðað hvaða áttir notandinn getur farið í og út frá því er prentað út hvert hann getur farið
+# Notandinn stimplar inn hvaða átt hann vill fara í og ef hún það er ekki hægt að fara í hana prentast út villa
+# Notandinn finnur leiðina sína til 3,1 og þegar hann kemst þangað vinnur hann
+
