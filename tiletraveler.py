f164d36452e99d0fc2224036d558be1218f77eac
Jóel Snær Garcia <joelsnaer1999@hotmail.com>
2018-09-18 09:46:29 +0000

    Búið til breytur og byrjað while loopu

--- tiletraveler.py
+++ tiletraveler.py
@@ -4,3 +4,17 @@
 # Notandinn stimplar inn hvaða átt hann vill fara í og ef hún það er ekki hægt að fara í hana prentast út villa
 # Notandinn finnur leiðina sína til 3,1 og þegar hann kemst þangað vinnur hann
 
+tile11 = "N"
+tile12 = "NES"
+tile13 = "ES"
+tile21 = "N"
+tile22 = "SW"
+tile23 = "EW"
+tile32 = "NS"
+tile33 = "SW"
+player_tile_x = 1
+player_tile_y = 1
+running = True
+
+while running:
+    
