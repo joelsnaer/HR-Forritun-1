ccac89512451d7a9c006037d5f8e58f0a1610c02
Jóel Snær Garcia <joelsnaer1999@hotmail.com>
2018-09-18 11:17:32 +0000

    Svarað spurningum

--- tiletraveler.py
+++ tiletraveler.py
@@ -4,6 +4,23 @@
 # Notandinn stimplar inn hvaða átt hann vill fara í og ef hún það er ekki hægt að fara í hana prentast út villa
 # Notandinn finnur leiðina sína til 3,1 og þegar hann kemst þangað vinnur hann
 # https://github.com/joelsnaer/HR-Forritun-1
+#
+# 1. Which implementation was easier and why?
+#Seinni framkvæmdin var léttari, fyrri framkvæmdin var erfiðara að lesa útaf þetta var ekki skipt upp í functions,
+#það var léttara að gera breytingar við öll output-in þegar það var einfaldara að sjá hvað gerði hvað.
+#
+#  2. Which implementation is more readable and why?
+#Seinni framkvæmdin er lesanlegri, hún er skipt upp í functions þannig það er léttar að lesa kóða þegar hann 
+#er skiptur upp í búta sem gera sína vinnu, ég get séð léttilega hvað þessi hlutur gerir og þannig tengt það 
+# inn í stóru myndina af forritinu
+#
+#  3. Which problems in the first implementations were you able to solve with the latter implementation?
+#Stærsta vandamálið með þennan kóða var hversu mikið erfiðara það er að lesa hann, 
+#vandamálin koma líka í að validation-ið þegar þú ert að stimpla inn breytu tekur lengri tíma og er mjög illa sett upp. 
+#að finna hvaða átt hver kassi getur farið í er bara ljótur kassi af breytum sem ég náði að taka út í seinni framkvæmdinni.
+#Ég lenti ekki í neinum stórum hættilegum vandamálum í fyrstu framkvæmdinni, 
+#þetta voru bara minor hlutir sem bætast upp í að láta kóðann vera verri.
+
 
 tile11 = "(N)orth."
 tile12 = "(N)orth or (E)ast or (S)outh."
@@ -50,7 +67,7 @@ while running:
             elif player_choice == "(S)":
                 player_tile_y -= 1
                 validation = 1
-            elif player_choice == "(W)":
+            elif player_choice == "(W)":   
                 player_tile_x -= 1
                 
         else:
